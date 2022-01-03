9929bb19982b6d514a3604e9cc60a2fad71be688
Divyanshu Ranjan Srivastava <greatdrs@gmail.com>
2022-01-03 09:53:07 +0530

    added circular singly linked list

--- Java2/src/com/circularSinglyLinkedList/Main.java
+++ Java2/src/com/circularSinglyLinkedList/Main.java
@@ -4,7 +4,13 @@ public class Main {
     public static void main(String[] args) {
         var cSLL = new CircularSinglyLinkedList();
         cSLL.createCSLL(5);
-        System.out.println(cSLL.head.value);
+
         System.out.println(cSLL.head.next.value);
+        cSLL.insertCSLL(4, 0);
+        cSLL.insertCSLL(1, 1);
+        cSLL.insertCSLL(8, 2);
+        cSLL.insertCSLL(9, 99);
+
+        cSLL.traverseCSLL();
     }
 }
